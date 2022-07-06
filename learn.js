bffd97c43b8bb18f2dba1492dceaaec9e7a2060a
Anton Zhuravlev <anton.zhuravlev21@gmail.com>
2022-07-06 18:05:10 +0600

    main

--- learn.js
+++ learn.js
@@ -1973,3 +1973,49 @@ class Rabbit2 extends Animal2 {
 let rabbit2 = new Rabbit2("Белый кролик"); 
 console.log(rabbit2.name);
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+//Статические свойства и методы
+
+class Article {
+  constructor(tittle, date) {
+    this.tittle = tittle;
+    this.date = date;
+  }
+
+  static compare(articleA, articleB) {
+    return articleA.date - articleB.date;
+  }
+  
+}
+
+//использование 
+let articles = [
+    new Article("HTML", new Date(2021, 1, 1)),
+    new Article("CSS", new Date(2022, 0, 1)),
+    new Article("JavaScript", new Date(2022, 11, 1))
+];
+
+articles.sort(Article.compare);
+console.log(articles[0].tittle); //HTML
+
+
+
+class Article2 {
+  constructor(title, date) {
+    this.title = title;
+    this.date = date;
+  }
+
+  static createTodays() {
+    // помним, что this = Article
+    return new this("Сегодняшний дайджест", new Date());
+  }
+}
+
+
+let article = Article2.createTodays();
+console.log(article.title); // Сегодняшний дайджест
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
